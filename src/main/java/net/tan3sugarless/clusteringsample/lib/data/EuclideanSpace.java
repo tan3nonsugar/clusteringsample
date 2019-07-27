9878c409cc6ec24e9069b93f0d0bf44b077a0da7
tan3nonsugar <tan3sugarless@gmail.com>
2019-07-27 13:39:29 +0900

    データ集合の保存時に次元も登録できるように

--- src/main/java/net/tan3sugarless/clusteringsample/lib/data/EuclideanSpace.java
+++ src/main/java/net/tan3sugarless/clusteringsample/lib/data/EuclideanSpace.java
@@ -8,6 +8,7 @@ import net.tan3sugarless.clusteringsample.exception.DimensionNotUnifiedException
 import net.tan3sugarless.clusteringsample.exception.NullCoordinateException;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * ユークリッド距離空間上の座標集合
@@ -19,9 +20,10 @@ import java.util.List;
 public class EuclideanSpace {
 
     private final List<List<Double>> points;
+    private final int dimension;
 
     /**
-     * n次元座標のリストをセットする
+     * n次元座標のリストと次元をセットする
      *
      * DimensionNotUnifiedException
      * 座標の次元が統一されていないリストをセットした
@@ -38,10 +40,11 @@ public class EuclideanSpace {
         if(points.stream().mapToInt(List::size).distinct().count()>1){
             throw new DimensionNotUnifiedException();
         }
-        if(points.stream().anyMatch(point -> point.stream().anyMatch(x -> x == null))){
+        if(points.stream().anyMatch(point -> point.stream().anyMatch(Objects::isNull))){
             throw new NullCoordinateException();
         }
 
         this.points = points;
+        this.dimension = points.stream().findFirst().map(List::size).orElse(0);
     }
 }
--- src/test/java/net/tan3sugarless/clusteringsample/lib/data/EuclideanSpaceTest.java
+++ src/test/java/net/tan3sugarless/clusteringsample/lib/data/EuclideanSpaceTest.java
@@ -7,43 +7,48 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class EuclideanSpaceTest {
+class EuclideanSpaceTest {
 
     //全体 null,空,要素1つ,要素複数
     //各要素 null含む,空含む,すべて空(0次元),1次元,n次元
     //各要素内の座標 null含む,0含む,null含まない
     //次元チェック すべて同じ次元,異なる次元
-    static Stream<Arguments> testConstructorProvider(){
+    static Stream<Arguments> testConstructorProvider() {
+        //@formatter:off
         return Stream.of(
-            Arguments.of(null,new NullPointerException()),
-            Arguments.of(Collections.emptyList(),null),
-            Arguments.of(Arrays.asList(Arrays.asList(1.5,-2.1)),null),
-            Arguments.of(Arrays.asList(Arrays.asList(1.2,0.1),Arrays.asList(0.0,1.5)),null),
-            Arguments.of(Arrays.asList(null,Arrays.asList(0,1.5),Arrays.asList(-0.9,0.1)),new NullPointerException()),
-            Arguments.of(Arrays.asList(Arrays.asList(-0.9,0.1),Arrays.asList(0.0,1.5),Collections.emptyList()),new DimensionNotUnifiedException()),
-            Arguments.of(Arrays.asList(Collections.emptyList(),Collections.emptyList(),Collections.emptyList()),null),
-            Arguments.of(Arrays.asList(Arrays.asList(1.5),Arrays.asList(0.0),Arrays.asList(-2.2)),null),
-            Arguments.of(Arrays.asList(Arrays.asList(1.5,2.2,-1.9),Arrays.asList(0.0,0.0,0.0),Arrays.asList(0.9,5.0,2.2)),null),
-            Arguments.of(Arrays.asList(Arrays.asList(1.5,null,-1.9),Arrays.asList(0.0,0.0,0.0),Arrays.asList(0.9,5.0,2.2)),new NullCoordinateException()),
-            Arguments.of(Arrays.asList(Arrays.asList(1.5,2.1,-1.9),Arrays.asList(0.0,0.0),Arrays.asList(0.9,5.0,2.2)),new DimensionNotUnifiedException()),
-            Arguments.of(Arrays.asList(Arrays.asList(2.1,-1.9),Arrays.asList(0,0,0),Arrays.asList(0.9,5.0,2.2)),new DimensionNotUnifiedException())
-
+                Arguments.of(null, new NullPointerException(), 0),
+                Arguments.of(Collections.emptyList(), null, 0),
+                Arguments.of(Arrays.asList(Arrays.asList(1.5, -2.1)), null, 2),
+                Arguments.of(Arrays.asList(Arrays.asList(1.2, 0.1), Arrays.asList(0.0, 1.5)), null, 2),
+                Arguments.of(Arrays.asList(null, Arrays.asList(0, 1.5), Arrays.asList(-0.9, 0.1)), new NullPointerException(), 0),
+                Arguments.of(Arrays.asList(Arrays.asList(-0.9, 0.1), Arrays.asList(0.0, 1.5), Collections.emptyList()), new DimensionNotUnifiedException(), 0),
+                Arguments.of(Arrays.asList(Collections.emptyList(), Collections.emptyList(), Collections.emptyList()), null, 0),
+                Arguments.of(Arrays.asList(Arrays.asList(1.5), Arrays.asList(0.0), Arrays.asList(-2.2)), null, 1),
+                Arguments.of(Arrays.asList(Arrays.asList(1.5, 2.2, -1.9), Arrays.asList(0.0, 0.0, 0.0), Arrays.asList(0.9, 5.0, 2.2)), null, 3),
+                Arguments.of(Arrays.asList(Arrays.asList(1.5, null, -1.9), Arrays.asList(0.0, 0.0, 0.0), Arrays.asList(0.9, 5.0, 2.2)), new NullCoordinateException(), 0),
+                Arguments.of(Arrays.asList(Arrays.asList(1.5, 2.1, -1.9), Arrays.asList(0.0, 0.0), Arrays.asList(0.9, 5.0, 2.2)), new DimensionNotUnifiedException(), 0),
+                Arguments.of(Arrays.asList(Arrays.asList(2.1, -1.9), Arrays.asList(0, 0, 0), Arrays.asList(0.9, 5.0, 2.2)), new DimensionNotUnifiedException(), 0)
         );
+        //@formatter:on
     }
 
     @ParameterizedTest
     @MethodSource("testConstructorProvider")
     @DisplayName("コンストラクタのテスト")
-    void testConstructor(List<List<Double>> points, RuntimeException e){
-        if(e==null){
-            Assertions.assertDoesNotThrow(()->new EuclideanSpace(points));
-        }else{
-            Assertions.assertThrows(e.getClass(),()->new EuclideanSpace(points));
+    void testConstructor(List<List<Double>> points, RuntimeException e, int dimension) {
+        if (e == null) {
+            Assertions.assertDoesNotThrow(() -> new EuclideanSpace(points));
+
+            EuclideanSpace actual = new EuclideanSpace(points);
+            Assertions.assertEquals(dimension, actual.getDimension());
+        } else {
+            Assertions.assertThrows(e.getClass(), () -> new EuclideanSpace(points));
         }
     }
 }
